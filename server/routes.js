8887823cc142b10347b7e2fce17ebe132619457a
Samuel Souza <samuef339@gmail.com>
2024-02-29 19:13:12 -0300

    controller

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const companysController = require('./Controllers/CompanysController')
+
 const router = express.Router();
 const guestRouter = express.Router();
 const authRouter = express.Router();
+
+
+guestRouter.post('/user/form', companysController.register);
+guestRouter.post('/user/auten', companysController.login);
 
 
 router.use('/a', authRouter);
